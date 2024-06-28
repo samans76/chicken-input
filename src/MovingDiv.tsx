328338165ac8d3aeac8c104b45d0f3ca39988f47
samans76 <samans767676gmail.com>
2024-06-29 03:20:47 +0330

    update moving element

--- src/MovingDiv.tsx
+++ src/MovingDiv.tsx
@@ -10,24 +10,59 @@ type MovingElementProps = {
   horizontalMove: number;
   verticalMove: number;
   duration: number;
-  isLoop?: boolean;
   style?: React.CSSProperties;
+  animationFPS?: number;
+  destroyAtEnd?: boolean;
 };
 
-function MovingElement({ children, horizontalMove, verticalMove, duration, isLoop, style }: MovingElementProps) {
-  const [startPosition, setStartPosition] = useState<Point | null>();
+const intervals: number[] = [];
+
+function MovingElement({
+  children,
+  horizontalMove,
+  verticalMove,
+  duration,
+  style,
+  animationFPS = 20,
+  destroyAtEnd,
+}: MovingElementProps) {
+  const [startPosition, setStartPosition] = useState<Point | null>(null);
+  const [position, setPosition] = useState<Point | null>(startPosition);
+  const bigDistance = horizontalMove * horizontalMove + verticalMove * verticalMove;
+  const [lastDistanceToEnd, setLastDistanceToEnd] = useState<number>(bigDistance);
   const ref = useRef<HTMLInputElement>(null);
-  console.log("rect : ", ref.current?.getBoundingClientRect().x);
+
   useEffect(() => {
-    setStartPosition({
+    const startPosition = {
       x: ref.current?.getBoundingClientRect()?.x ?? -100,
       y: ref.current?.getBoundingClientRect().y ?? -100,
-    });
-    // component moving should reset
-    const intervalTime = 10; // base on duration
-    setInterval(() => {
-      //
+    };
+    setStartPosition(startPosition);
+    setPosition(startPosition);
+    intervals.forEach((i) => clearInterval(i));
+
+    const intervalTime = 1000 / animationFPS;
+    const interval = setInterval(() => {
+      if (!position) return;
+      const endX = startPosition.x + horizontalMove;
+      const endY = startPosition.y + verticalMove;
+      const distanceToEnd = calculateDistance(position.x, endX, position.y, endY);
+      let passedEndPoint = distanceToEnd > lastDistanceToEnd;
+      if (passedEndPoint) {
+        intervals.forEach((i) => clearInterval(i));
+        if (destroyAtEnd) setPosition({ x: -10000, y: -10000 });
+      }
+      setLastDistanceToEnd(distanceToEnd);
+
+      const intervalCounts = duration / intervalTime;
+      const intervalHorizontalMove = horizontalMove / intervalCounts;
+      const intervalVerticalMove = verticalMove / intervalCounts;
+      if (position) {
+        const newPosition = { x: position.x + intervalHorizontalMove, y: position.y + intervalVerticalMove };
+        setPosition(newPosition);
+      }
     }, intervalTime);
+    intervals.push(interval);
   }, [children]);
 
   return (
@@ -35,10 +70,7 @@ function MovingElement({ children, horizontalMove, verticalMove, duration, isLoo
       ref={ref}
       style={{
         ...style,
-        position: "absolute",
-        top: 0,
-        right: -50,
-        transform: `translate(${horizontalMove}px, ${verticalMove}px)`,
+        transform: `translate(${position?.x}px, ${position?.y}px)`,
       }}
     >
       {children}
@@ -47,3 +79,9 @@ function MovingElement({ children, horizontalMove, verticalMove, duration, isLoo
 }
 
 export default MovingElement;
+
+function calculateDistance(x1: number, x2: number, y1: number, y2: number): number {
+  const dx = x2 - x1;
+  const dy = y2 - y1;
+  return Math.sqrt(dx * dx + dy * dy);
+}
